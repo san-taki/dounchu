c25a308440968864756c00386e44f8a689cb3f11
santaki <stakidroid@gmail.com>
2023-09-04 20:51:37 +0900

    add lint tsconfig package

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'prettier',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+}
